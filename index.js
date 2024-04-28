75f99ce02c19e3a6d104c0ee497720ed3cd8b729
Marco Parra <mpdragon123@gmail.com>
2024-04-28 12:36:59 -0300

    Finished project

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const menuButton = document.querySelector(".menu-button");
 menuButton.addEventListener("click", () => {
     navbar.classList.toggle("show-menu");
 });
+
+
+  const botao = document.getElementById("btnVerAgora"); // Referência ao botão
+  const secaoDestino = document.getElementById("selecao"); // Referência à seção de destino
+  
+  botao.addEventListener("click", () => {
+    secaoDestino.scrollIntoView({ behavior: "smooth" }); // Rolagem suave para a seção
+  });
